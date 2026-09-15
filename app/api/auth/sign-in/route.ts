e7ebe158346a9d78cd78651f957d13b8cd67fd7a
Joylynne Esportuno <degozarunyan@gmail.com>
2026-09-15 19:05:06 +0800

    redeploy

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -26,6 +26,7 @@ export async function POST(req: NextRequest) {
     });
 
     if (error) {
+      console.error(error)
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
 
