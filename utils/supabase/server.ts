d610618f7b4a901fc8420186cc9a7de895ee76a8
Joylynne Esportuno <degozarunyan@gmail.com>
2026-09-15 23:03:34 +0800

    redeploy

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -3,6 +3,7 @@ import { createServerClient } from "@supabase/ssr";
 export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 export const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
+
 export const createClient = (cookieStore: any) =>
   createServerClient(supabaseUrl!, supabaseKey!, {
     cookies: {
